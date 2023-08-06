abdcd3e3be80ad439008b68c3c96413a250039cb
Arnaldo Neto <vilela-neto@hotmail.com>
2023-08-06 00:05:02 -0300

    4. Drop-Down Lists - Part 2

--- src/main/java/com/luv2code/jsf/hello/StudentTwo.java
+++ src/main/java/com/luv2code/jsf/hello/StudentTwo.java
@@ -1,5 +1,8 @@
 package com.luv2code.jsf.hello;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean
@@ -9,12 +12,30 @@ public class StudentTwo {
 	private String lastName;
 	private String country;
 	
+	// list of countries for the drop-down list
+	List<String> countryOptions;
+	
 	// no-arg constructor
 	public StudentTwo() {
+		// populate the list of countries
+		countryOptions = new ArrayList<>();
 		
+		countryOptions.add("Brazil");
+		countryOptions.add("France");
+		countryOptions.add("Germany");
+		countryOptions.add("India");
+		countryOptions.add("Turkey");
+		countryOptions.add("United Kingdom");
+		countryOptions.add("United States");
 	}
 
 	// getter/setter methods
+	
+	// need to add a getter method for country options
+	public List<String> getCountryOptions() {
+		return countryOptions;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
