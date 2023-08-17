696393fc0cf578f6f091aa47f0aa8d77e5aebc08
Arnaldo Neto <vilela-neto@hotmail.com>
2023-08-17 15:50:11 -0300

    3.7. Pre-populating JSF Forms

---
+++ src/main/java/com/luv2code/jsf/hello/StudentFive.java
@@ -0,0 +1,45 @@
+package com.luv2code.jsf.hello;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class StudentFive {
+
+	private String firstName;
+	private String lastName;
+	private String favoriteLanguage;
+	
+	public StudentFive() {
+		
+		// pre-populate the bean
+		firstName = "Mary";
+		lastName = "Public";
+		favoriteLanguage = "Ruby";
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+	
+	
+}
