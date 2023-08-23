bc7f721963907a53de2f58c1b37e28b2a7d987a7
Arnaldo Neto <vilela-neto@hotmail.com>
2023-08-23 00:42:47 -0300

    New Project Created

---
+++ src/main/java/com/luv2code/jsf/beandemo/TourBean.java
@@ -0,0 +1,30 @@
+package com.luv2code.jsf.beandemo;
+
+import javax.annotation.ManagedBean;
+
+@ManagedBean
+public class TourBean {
+
+	private String kindOfTour;
+	
+	public TourBean() {
+		
+	}
+		
+	public String getKindOfTour() {
+		return kindOfTour;
+	}
+
+	public void setKindOfTour(String kindOfTour) {
+		this.kindOfTour = kindOfTour;
+	}
+
+	public String startTheTour() {
+		if (kindOfTour != null && kindOfTour.equals("city")) {
+			return "city_tour";
+		}
+		else {
+			return "country_tour";
+		}
+	}
+}
