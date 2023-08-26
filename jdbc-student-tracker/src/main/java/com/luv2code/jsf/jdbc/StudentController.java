1d0a4e0915b0b8d75e6513df95d5fda8943cd4dd
Arnaldo Neto <vilela-neto@hotmail.com>
2023-08-26 17:05:13 -0300

    7.9. Sample Application Architecture - Part 2

---
+++ jdbc-student-tracker/src/main/java/com/luv2code/jsf/jdbc/StudentController.java
@@ -0,0 +1,56 @@
+package com.luv2code.jsf.jdbc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class StudentController {
+
+	private List<Student> students;
+	private StudentDbUtil studentDbUtil;
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	public StudentController() throws Exception {
+		students = new ArrayList<>();
+		
+		studentDbUtil = StudentDbUtil.getInstance();
+	}
+	
+	public List<Student> getStudents() {
+		return students;
+	}
+	
+	public void loadStudents() {
+		
+		logger.info("Loading students");
+		
+		students.clear();
+		
+		try {
+			
+			// get all students from database
+			students = studentDbUtil.getStudents();
+			
+		} catch (Exception exc) {
+			// send this to server logs
+			logger.log(Level.SEVERE, "Error loading students", exc);
+			
+			// add error message for JSF page
+			addErrorMessage(exc);
+		}
+	}
+	
+	private void addErrorMessage(Exception exc) {
+		FacesMessage message = new FacesMessage("Error: " + exc.getMessage());
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+	
+}
