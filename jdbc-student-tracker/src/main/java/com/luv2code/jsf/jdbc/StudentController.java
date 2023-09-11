7d513d0f6b9f00e9e473a86560f153135051c2b5
Arnaldo Neto <vilela-neto@hotmail.com>
2023-09-11 16:30:29 -0300

    7.14. JDBC - Update Student in the Database - Part 2

--- jdbc-student-tracker/src/main/java/com/luv2code/jsf/jdbc/StudentController.java
+++ jdbc-student-tracker/src/main/java/com/luv2code/jsf/jdbc/StudentController.java
@@ -2,12 +2,14 @@ package com.luv2code.jsf.jdbc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
 @ManagedBean
@@ -69,7 +71,57 @@ public class StudentController {
 		
 		return "list-students?faces-redirect=true";
 	}
+	
+	public String loadStudent(int studentId) {
 		
+		logger.info("loading student: " + studentId);
+		
+		try {
+			// get student from database
+			Student theStudent = studentDbUtil.getStudent(studentId);
+			
+			// put in the request attribute ... so we can use it on the form page
+			ExternalContext externalContext = 
+					FacesContext.getCurrentInstance().getExternalContext();
+			
+			Map<String, Object> requestMap = externalContext.getRequestMap();
+			requestMap.put("student", theStudent);
+			
+		} catch (Exception exc) {
+			// send this to server logs
+			logger.log(Level.SEVERE, "Error loading student id:" + studentId, exc);	
+		
+			// add error message for JSF page
+			addErrorMessage(exc);
+			
+			return null;
+		}
+		
+		return "update-student-form.xhtml";
+	}
+		
+	public String updateStudent(Student theStudent) {
+
+		logger.info("updating student: " + theStudent);
+		
+		try {
+			
+			// update student in the database
+			studentDbUtil.updateStudent(theStudent);
+			
+		} catch (Exception exc) {
+			// send this to server logs
+			logger.log(Level.SEVERE, "Error updating student: " + theStudent, exc);
+			
+			// add error message for JSF page
+			addErrorMessage(exc);
+			
+			return null;
+		}
+		
+		return "list-students?faces-redirect=true";
+	}
+	
 	private void addErrorMessage(Exception exc) {
 		FacesMessage message = new FacesMessage("Error: " + exc.getMessage());
 		FacesContext.getCurrentInstance().addMessage(null, message);
--- jdbc-student-tracker/src/main/java/com/luv2code/jsf/jdbc/StudentDbUtil.java
+++ jdbc-student-tracker/src/main/java/com/luv2code/jsf/jdbc/StudentDbUtil.java
@@ -13,7 +13,7 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class StudentDbUtil {
-	
+
 	private static StudentDbUtil instance;
 	private DataSource dataSource;
 	private String jndiName = "java:comp/env/jdbc/student_tracker";
@@ -26,7 +26,7 @@ public class StudentDbUtil {
 		return instance;
 	}
 	
-	private StudentDbUtil() throws Exception {
+	private StudentDbUtil() throws Exception {		
 		dataSource = getDataSource();
 	}
 
@@ -37,24 +37,24 @@ public class StudentDbUtil {
 		
 		return theDataSource;
 	}
-	
+		
 	public List<Student> getStudents() throws Exception {
-		
+
 		List<Student> students = new ArrayList<>();
-		
+
 		Connection myConn = null;
 		Statement myStmt = null;
 		ResultSet myRs = null;
 		
 		try {
 			myConn = getConnection();
-			
+
 			String sql = "select * from student order by last_name";
-			
+
 			myStmt = myConn.createStatement();
-			
+
 			myRs = myStmt.executeQuery(sql);
-			
+
 			// process result set
 			while (myRs.next()) {
 				
@@ -63,15 +63,16 @@ public class StudentDbUtil {
 				String firstName = myRs.getString("first_name");
 				String lastName = myRs.getString("last_name");
 				String email = myRs.getString("email");
-				
+
 				// create new student object
-				Student tempStudent = new Student(id, firstName, lastName, email);
-				
+				Student tempStudent = new Student(id, firstName, lastName,
+						email);
+
 				// add it to the list of students
 				students.add(tempStudent);
 			}
-
-			return students;
+			
+			return students;		
 		}
 		finally {
 			close (myConn, myStmt, myRs);
@@ -79,47 +80,48 @@ public class StudentDbUtil {
 	}
 
 	public void addStudent(Student theStudent) throws Exception {
-		
+
 		Connection myConn = null;
 		PreparedStatement myStmt = null;
-		
+
 		try {
 			myConn = getConnection();
-			
+
 			String sql = "insert into student (first_name, last_name, email) values (?, ?, ?)";
-			
+
 			myStmt = myConn.prepareStatement(sql);
-			
+
 			// set params
 			myStmt.setString(1, theStudent.getFirstName());
 			myStmt.setString(2, theStudent.getLastName());
 			myStmt.setString(3, theStudent.getEmail());
 			
-			myStmt.execute();
+			myStmt.execute();			
 		}
 		finally {
-			close(myConn, myStmt);
+			close (myConn, myStmt);
 		}
 		
 	}
-
+	
 	public Student getStudent(int studentId) throws Exception {
+	
 		Connection myConn = null;
 		PreparedStatement myStmt = null;
 		ResultSet myRs = null;
 		
 		try {
 			myConn = getConnection();
-			
+
 			String sql = "select * from student where id=?";
-			
+
 			myStmt = myConn.prepareStatement(sql);
 			
 			// set params
 			myStmt.setInt(1, studentId);
 			
 			myRs = myStmt.executeQuery();
-			
+
 			Student theStudent = null;
 			
 			// retrieve data from result set row
@@ -128,34 +130,35 @@ public class StudentDbUtil {
 				String firstName = myRs.getString("first_name");
 				String lastName = myRs.getString("last_name");
 				String email = myRs.getString("email");
-				
-				theStudent = new Student(id, firstName, lastName, email);
+
+				theStudent = new Student(id, firstName, lastName,
+						email);
 			}
 			else {
 				throw new Exception("Could not find student id: " + studentId);
 			}
-			
+
 			return theStudent;
 		}
 		finally {
-			close(myConn, myStmt, myRs);
+			close (myConn, myStmt, myRs);
 		}
 	}
 	
 	public void updateStudent(Student theStudent) throws Exception {
-		
+
 		Connection myConn = null;
 		PreparedStatement myStmt = null;
-		
+
 		try {
 			myConn = getConnection();
-			
+
 			String sql = "update student "
 						+ " set first_name=?, last_name=?, email=?"
 						+ "where id=?";
-			
+
 			myStmt = myConn.prepareStatement(sql);
-			
+
 			// set params
 			myStmt.setString(1, theStudent.getFirstName());
 			myStmt.setString(2, theStudent.getLastName());
@@ -165,62 +168,61 @@ public class StudentDbUtil {
 			myStmt.execute();
 		}
 		finally {
-			close(myConn, myStmt);
+			close (myConn, myStmt);
 		}
 		
 	}
 	
 	public void deleteStudent(int studentId) throws Exception {
-		
+
 		Connection myConn = null;
 		PreparedStatement myStmt = null;
-		
+
 		try {
 			myConn = getConnection();
-			
+
 			String sql = "delete from student where id=?";
-			
+
 			myStmt = myConn.prepareStatement(sql);
-			
+
 			// set params
 			myStmt.setInt(1, studentId);
 			
 			myStmt.execute();
 		}
 		finally {
-			close(myConn, myStmt);
-		}
-	}
+			close (myConn, myStmt);
+		}		
+	}	
 	
 	private Connection getConnection() throws Exception {
-		
+
 		Connection theConn = dataSource.getConnection();
 		
 		return theConn;
 	}
 	
-
 	private void close(Connection theConn, Statement theStmt) {
 		close(theConn, theStmt, null);
 	}
 	
 	private void close(Connection theConn, Statement theStmt, ResultSet theRs) {
-		
+
 		try {
 			if (theRs != null) {
 				theRs.close();
 			}
-			
-			if(theStmt != null) {
+
+			if (theStmt != null) {
 				theStmt.close();
 			}
-			
-			if(theConn != null) {
+
+			if (theConn != null) {
 				theConn.close();
 			}
 			
 		} catch (Exception exc) {
 			exc.printStackTrace();
 		}
-	}
+	}	
 }
